362b46550bbe917a942ab1aaee7f9e0e248f9f17
carloDM <carlo.de.mauro.work@gmail.com>
2023-06-14 15:56:31 +0200

    layout & header

---
+++ resources/views/partial/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+  <div class="container">
+      <ul class="nav">
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">artisti</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">musei</a>
+          </li>
+
+        </ul>
+  </div>
+</header>
