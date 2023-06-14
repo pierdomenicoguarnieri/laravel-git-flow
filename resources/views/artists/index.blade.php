2c63f54a4dead8140f77619b09e04ba353106642
David Calaiò <davidcjr11@gmail.com>
2023-06-14 17:28:06 +0200

    closed create artists scaffholding

--- resources/views/artists/index.blade.php
+++ resources/views/artists/index.blade.php
@@ -9,5 +9,25 @@
     <div class="container my-5">
       <h1>Index Artisti</h1>
     </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Cognome</th>
+          <th scope="col">Azioni</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          @foreach ($artists as $artist )
+            <th>{{ $artist['id'] }}</th>
+            <td>{{ $artist['name'] }}</td>
+            <td>{{ $artist['surname'] }}</td>
+            <td><a class="btn" href="{{route('artists.show')}}">Dettagli</a></td>
+          @endforeach
+        </tr>
+      </tbody>
+    </table>
   </main>
 @endsection
