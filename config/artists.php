782e73682200bcf653f185c70e50950b52db5373
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-14 17:02:16 +0200

    changed: merged 2 .php files

--- config/artists.php
+++ config/artists.php
@@ -66,5 +66,64 @@ return [
 
     Esattamente l'anno dopo la Carbentus diede alla luce il primo figlio, il futuro pittore, che verrà battezzato Vincent Willem in ricordo del defunto fratellino: «fin dal primo giorno, quindi, la vita di Vincent fu segnata da una triste coincidenza» commenta il critico Rainer Metzger «numerosi psicologi, per contro, non mancarono di sottolineare che questo bambino, in un certo senso, era venuto al mondo nell'anniversario della morte di suo fratello e videro in ciò la radice dell'inclinazione dell'artista al paradosso». Theodorus e Cornelia, in ogni caso, ebbero altri cinque figli: Anna Cornelia (1855-1930), Théodorus junior, ben presto denominato semplicemente Théo (1º maggio 1857-25 gennaio 1891), Elisabeth (1859-1936), Wilhelmina Jacoba (1862-1941) e Cornelis (1867-1900). Dal gennaio 1861 al settembre 1864 Vincent van Gogh studiò alla scuola del paese e dal 1º ottobre 1864 frequentò un collegio della vicina Zevenbergen, dove apprese il francese, l'inglese, il tedesco e l'arte del disegno. Dal 1866 frequentò un'altra scuola presbiteriale, la scuola secondaria di Tilburg, con professore di arte il pittore Constant Cornelis Huijsmans. Ma il 19 marzo 1868, a causa dello scarso rendimento nonché per via di alcuni problemi economici sofferti dal padre, ritornò a Zundert senza aver concluso gli studi. Furono tutto sommato anni felici e lieti che verranno ricordati da Vincent con grande nostalgia, come leggiamo nella lettera 573: "
   ],
+  [
+    'name'=> ' Leonardo',
+    'surname'=> 'Da Vinci',
+    'date_of_birth'=> '1452-03-15',
+    'date_of_death'=> '1519-05-02',
+    'place_of_death'=> 'Amboise',
+    'profession'=> [
+      "pittore",
+      "ingegnere",
+      "scienziato",
+      "scultore",
+      "insegnante",
+  ],
+    'art_movement'=> 'Rinascimento',
+    'works'=>  [
+      "la gioconda",
+      "Madonna del Garofano ",
+      "Annunciazione",
+      "L ultima cena",
+  ],
+    'biography'=> 'Fu tanto raro e universale, che dalla natura per suo miracolo esser produtto dire si puote: la quale non solo della bellezza del corpo, che molto bene gli concedette, volse dotarlo, ma di molte rare virtù volse anchora farlo maestro. Assai valse in matematica et in prospettiva non meno, et operò di scultura, et in disegno passò di gran lunga tutti li altri. Hebbe bellissime inventioni, ma non colorì molte cose, perché si dice mai a sé medesimo avere satisfatto, et però sono tante rare le opere sue.',
+],
+  [
+    'name'=> ' Keith',
+    'surname'=> 'Haring',
+    'date_of_birth'=> '1958-03-04',
+    'date_of_death'=> '1990-05-02',
+    'place_of_death'=> 'New York',
+    'profession'=> [
+      "street artist",
+  ],
+    'art_movement'=> 'pop art',
+    'works'=>  [
+      "A New Dimension",
+      "Kunsthalle Wien",
+      "Van de Weghe Fine Art",
+      "Editions on Paper",
+  ],
+    'biography'=> 'Mio padre realizzava per me personaggi dei cartoni animati, e questi erano simili a come disegnavo io - con un unica linea e un contorno fumettistico',
+],
+
+  [
+    'name'=> ' Gustav  ',
+    'surname'=> 'Klimt ',
+    'date_of_birth'=> '1862-07-14',
+    'date_of_death'=> '1918-02-06',
+    'place_of_death'=> 'Vienna',
+    'profession'=> [
+      "Painter",
+  ],
+    'art_movement'=> 'secessionismo austriaco',
+    'works'=>  [
+      "Il bacio",
+      "amore e psyche",
+      "Nuda Veritas",
+      "Ritratto di signora",
+  ],
+    'biography'=> "Gustav Klimt nacque il 14 luglio 1862 a Baumgarten, allora sobborgo di Vienna, secondo di sette fratelli (quattro femmine e tre maschi). Il padre Ernst Klimt (1834-1892), nativo della Boemia, era un orafo;[2] la madre, Anna Finster (1836-1915), era una donna colta e versata nella musica lirica. Tutti i figli maschi della famiglia Klimt riveleranno in futuro una forte inclinazione per l'arte. I fratelli minori di Gustav, Ernst e Georg, diverranno anch'essi artisti: Ernst diventerà un pittore e lavorerà con il fratello Gustav fino alla sua morte (il 9 dicembre del 1895 all'età di 28 anni), Georg seguirà le orme del padre, diventando orefice. Gustav frequentò per otto anni la scuola primaria nel settimo distretto comunale di Vienna e successivamente, nel 1876, malgrado le pressanti ristrettezze economiche, il quattordicenne Gustav venne ammesso a frequentare la scuola d'arte e mestieri dell'Austria Kunstgewerbeschule, dove studiò arte applicata fino al 1883, cominciando a formare personali orientamenti di gusto e imparò a padroneggiare diverse tecniche artistiche, dal mosaico alla ceramica, nel rispetto dei canoni accademici e della storia dell'arte del passato; fondamentale fu qui l influenza esercitata da Ferdinand Laufberger e Hans Makart, sui quali condusse i primi studi. ",
+]
 
 ];
