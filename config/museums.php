767615953bf042ce54e64dfbcbf2164c59b3bf87
marcocnc <marcocancelliere56@gmail.com>
2023-06-14 16:40:43 +0200

    created file museums in config

---
+++ config/museums.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+    [
+        'name' => 'Museo del cinema',
+        'founder' => 'Maria Adriana Prolo',
+        'construction_date' => '1958-01-01',
+        'number_of_visitors' => 700,
+        'number_of_departments' => 20,
+        'geographic_coordinates'=> '45°04\'10.35″N 7°41\'35.62\″E'
+    ],
+
+    [
+        'name' => 'Museo egizio',
+        'founder' => 'Evelina Christillin',
+        'construction_date' => '1824-01-01',
+        'number_of_visitors' => 500,
+        'number_of_departments' => 35,
+        'geographic_coordinates'=> '45°04\'06″N 7°41\'04″E'
+    ],
+
+    [
+        'name' => 'Museo Archeologico Nazionale di Napoli',
+        'founder' => 'Ferdinando IV',
+        'construction_date' => '1777-01-01',
+        'number_of_visitors' => 600,
+        'number_of_departments' => 27,
+        'geographic_coordinates'=> '40°51\'12.16″N 14°15\'01.75″E'
+    ],
+
+    [
+        'name' => 'Museo del Novecento',
+        'founder' => 'Gruppo Rota',
+        'construction_date' => '2010-12-06',
+        'number_of_visitors' => 600,
+        'number_of_departments' => 27,
+        'geographic_coordinates'=> '48°51\'17.18″N 14°20\'01.95″E'
+    ],
+
+    [
+        'name' => 'Museo del Louvre',
+        'founder' => 'Napoleone Bonaparte',
+        'construction_date' => '1793-12-06',
+        'number_of_visitors' => 1500,
+        'number_of_departments' => 27,
+        'geographic_coordinates'=> '40°51\'12.16″N 14°15\'01.75″E'
+    ],
+
+    [
+        'name' => 'Musei Vaticani',
+        'founder' => 'Papa Giulio II',
+        'construction_date' => '1506-12-06',
+        'number_of_visitors' => 1700,
+        'number_of_departments' => 17,
+        'geographic_coordinates'=> '50°81\'19.21″N 17°11\'06.44″E'
+    ],
+];
