a5c7c1c63cb208c189ebd34dbf7f8d272ba16e7b
carloDM <carlo.de.mauro.work@gmail.com>
2023-06-20 16:39:39 +0200

    recupero index artisti

--- resources/views/artists/index.blade.php
+++ resources/views/artists/index.blade.php
@@ -1,39 +1,33 @@
-@extends('museums.layout.main_museums')
+@extends('artists.layout.main_artists')
+
 @section('title')
   Index
 @endsection
+
 @section('content')
-  <main>
+  <main class="container">
     <div class="container my-5">
-      <h1>Index Musei</h1>
-      <h1 class="mb-2">Index Musei</h1>
-
-      <a href="{{route('museums.create')}}" class="btn btn-success mb-2">Crea un nuovo museo</a>
-
-      <div class="pg-table-wrapper">
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#ID</th>
-              <th scope="col">Name</th>
-              <th scope="col">Coordinate Geografiche</th>
-              <th scope="col">Azioni</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>
-                <a href="{{route('museums.show', 'ciao')}}" class="btn btn-primary">Vai</a>
-                <a href="{{route('museums.edit', 'ciao')}}" class="btn btn-warning">Modifica</a>
-                <a href="" class="btn btn-danger">Elimina</a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <h1>Index Artisti</h1>
     </div>
+    <table class="table ">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Cognome</th>
+          <th scope="col">Azioni</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($artists as $artist )
+        <tr>
+            <th>{{ $artist['id'] }}</th>
+            <td>{{ $artist['name'] }}</td>
+            <td>{{ $artist['surname'] }}</td>
+            <td><a class="btn" href="{{route('artists.show', $artist)}}">Dettagli</a></td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
   </main>
 @endsection
