b1efb5f8fa6d969917e7e5c8d96d36a9fd59be6b
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-20 16:24:24 +0200

    add: index artworks

--- resources/views/artworks/index.blade.php
+++ resources/views/artworks/index.blade.php
@@ -8,13 +8,17 @@
   <main class="container">
     <div class="container my-5">
       <h1>Index Opere D'Arte</h1>
+
+      <a href="{{route('artworks.create')}}" class="btn btn-success mb-2">Crea un nuovo museo</a>
+
     </div>
     <table class="table ">
       <thead>
         <tr>
           <th scope="col">ID</th>
-          <th scope="col">Nome</th>
-          <th scope="col">Cognome</th>
+          <th scope="col">Titolo</th>
+          <th scope="col">Artista</th>
+          <th scope="col">Anno</th>
           <th scope="col">Azioni</th>
         </tr>
       </thead>
@@ -25,7 +29,11 @@
             <td>{{ $artwork['title'] }}</td>
             <td>{{ $artwork['artist'] }}</td>
             <td>{{ $artwork['year'] }}</td>
-            <td><a class="btn" href="{{route('artworks.show', $artwork)}}">Dettagli</a></td>
+            <td>
+              <a href="{{route('artworks.show', $artwork)}}" class="btn btn-primary">Vai</a>
+              <a href="{{route('artworks.edit', $artwork)}}" class="btn btn-warning">Modifica</a>
+              <a href="" class="btn btn-danger">Elimina</a>
+            </td>
           </tr>
         @endforeach
       </tbody>
