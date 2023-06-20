eec36d98e860086afc03b9c7820f22128ead5586
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-20 15:35:28 +0200

    changed: yield name

--- resources/views/museums/layout/main_museums.blade.php
+++ resources/views/museums/layout/main_museums.blade.php
@@ -4,7 +4,7 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
 
-      <title>Dashboard Musei | @yield('title_museums')</title>
+      <title>Dashboard Musei | @yield('title')</title>
 
       <!-- Fonts -->
       <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -15,7 +15,7 @@
   <body>
     @include('partial.header')
 
-    @yield('content_museums')
+    @yield('content')
 
   </body>
 
--- resources/views/museums/show.blade.php
+++ resources/views/museums/show.blade.php
@@ -1,10 +1,10 @@
 @extends('museums.layout.main_museums')
 
-@section('title_museums')
+@section('title')
   Show
 @endsection
 
-@section('content_museums')
+@section('content')
   <main>
     <div class="container my-5">
       <h1 class="mb-2">Show {{$museum->name}}</h1>
