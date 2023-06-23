e20e9985ad10b9b865fa7d76be8d19febdd253a4
marcocnc <marcocancelliere56@gmail.com>
2023-06-23 16:38:40 +0200

    Closed #43 created one to many museum artworks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       $this->call([
+        ArtworksTableSeeder::class,
         ArtistTableSeeder::class,
-        MuseumTableSeeder::class,
-        ArtworksTableSeeder::class
+        MuseumTableSeeder::class
       ]);
     }
 }
